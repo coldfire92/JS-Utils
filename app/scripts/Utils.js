510767f75ba797943f07565377bbb1a43dd67ea7
Michal <kontakt@michal-pawelczyk.com>
2015-03-09 00:14:31 +0100

    Add method isIE to Utils

--- app/scripts/Utils.js
+++ app/scripts/Utils.js
@@ -29,6 +29,33 @@ define('Utils', [], function () {
 
 			window.location.href = url;
 
+		},
+
+
+		/**
+		 * Return is IE browser
+		 * http://stackoverflow.com/questions/4169160/javascript-ie-detection-why-not-use-simple-conditional-comments/16657946#16657946
+		 * 
+		 * @return {Boolean} 
+		 */
+		isIE : function(){
+
+			var undef,rv = -1; // Return value assumes failure.
+			var ua = window.navigator.userAgent;
+			var msie = ua.indexOf('MSIE ');
+			var trident = ua.indexOf('Trident/');
+
+			if (msie > 0) {
+				// IE 10 or older => return version number
+				rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+			} else if (trident > 0) {
+				// IE 11 (or newer) => return version number
+				var rvNum = ua.indexOf('rv:');
+				rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
+			}
+
+			return ((rv > -1) ? rv : undef);
+		
 		}
 
 
